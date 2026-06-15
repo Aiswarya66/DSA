d516e4503cb261daea898639f76593f80165c687
Aiswarya66 <63280290+Aiswarya66@users.noreply.github.com>
2026-06-15 08:40:37 +0530

    Optimize twoSum function using hash map
    Refactor twoSum function to use a hash map for improved performance.

--- Arrays/twoSum.ts
+++ Arrays/twoSum.ts
@@ -1,30 +1,16 @@
 function twoSum(nums: number[], target: number): number[] {
 
     let n = nums.length;
-    let result:number[];
-    for(let i = 0; i<n-1;i++){
-        let diff=target-nums[i];
-        //if(diff>-1){
-            //console.log(`nums slice: ${nums.slice(i+1)}`);
-            let j=findindex(nums.slice(i+1),diff);
-            //console.log(`i: ${i} j: ${j} diff: ${diff}`);
-            if(j>-1){return [i,j+i+1];}
-        //}
-        // i++;
-    }
-    
-    function findindex(a: number[], b: number):number{
-        let n2=a.length;
-        while(n2>0){
-            if(a[n2-1]==b){
-                console.log(`found ${b} at index ${n2-1}`);
-                return n2-1;
-            }
-            n2--;
+    const tMap:Record<number,number> = Object.create(null);
+    for(let i=0;i< n; i++){
+        const complement=target-nums[i];
+        const t = tMap[complement];
+        if(t!= undefined){
+            return [i, t]
         }
-        return -1;
+        tMap[nums[i]]=i;
     }
-    return result;
+    return [];
 };
 //console.log(`${twoSum([2,7,11,15],9)}`);
 //console.log(`${twoSum([3,2,4],6)}`);
